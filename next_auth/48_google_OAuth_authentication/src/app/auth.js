2681b2df1f7b41b1a9077a44039b90d3eb5a7c45
Tutul Kabir <tutulkabir@gmail.com>
2024-11-08 22:27:39 +0530

    google OAuth authentication

---
+++ next_auth/48_google_OAuth_authentication/src/app/auth.js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const { auth, handlers } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+      },
+      authorize: async (credentials) => {
+        const user = { id: 101, username: "tutul", password: "admin" };
+
+        if (
+          credentials?.username === user.username &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+
+  secret: process.env.AUTH_SECRET,
+});
---
+++ next_auth/48_google_OAuth_authentication/src/app/page.js
@@ -0,0 +1,25 @@
+import { redirect } from "next/navigation";
+import { auth } from "./auth";
+import Link from "next/link";
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return (
+    <>
+      <div>
+        <h3>Protected page</h3>
+        {session.user.name && <p>{session.user.name}</p>}
+        {session.user.email && <p>{session.user.email}</p>}
+        <Link
+          className="px-5 py-1 border border-green-400"
+          href={"api/auth/signout"}
+        >
+          Logout
+        </Link>
+      </div>
+    </>
+  );
+}
